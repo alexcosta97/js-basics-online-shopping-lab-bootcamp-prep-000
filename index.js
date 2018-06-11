3d3f34f559a4a27adef0b5180eb3eb45e9c379cb
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-11 22:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined)
+  {
+    return 'Sorry, we don\'t have a credit card on file for you.';
+  }
+  else
+  {
+    cart = [];
+    
+  }
 }
